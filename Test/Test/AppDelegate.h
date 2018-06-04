50ed5cfcba88885e6d01eaf084098ea668728914
luffy.huang <112233huangc0>
2018-06-04 16:20:35 +0800

    第一次提交

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 佐路飞 on 2018/6/4.
+//  Copyright © 2018年 Kucoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 佐路飞 on 2018/6/4.
+//  Copyright © 2018年 Kucoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
